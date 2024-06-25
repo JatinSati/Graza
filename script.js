1135b0aee25ea65147ae7b230bcdf0dc05a44442
jatinsati <satijatin09@gmail.com>
2024-06-25 18:29:13 +0530

    menu functionality

--- script.js
+++ script.js
@@ -22,3 +22,65 @@ thumbnails.forEach((thumbnail, index) => {
         showSlider();
     });
 });
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown =>{
+    const select = dropdown.querySelector('.select');
+    const icon = dropdown.querySelector('#icon');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    dropdown.addEventListener('click', ()=>{
+        const isOpen = menu.classList.contains('menu-open');
+        dropdown.classList.toggle('dropdown-expanded', !isOpen);
+        icon.classList.toggle('icon-rotate', !isOpen);
+        menu.classList.toggle('menu-open', !isOpen);
+    });
+    
+    options.forEach(option => {
+        option.addEventListener('click', (event) => {
+            event.stopPropagation();
+            selected.innerText = option.innerText;
+            icon.classList.remove('icon-rotate');
+            menu.classList.remove('menu-open');
+            dropdown.classList.remove('dropdown-expanded', 'bill-expand');
+            options.forEach(opt => {
+                opt.classList.remove('active', 'hidden');
+            });
+            option.classList.add('active', 'hidden');
+        });
+    });
+    document.addEventListener('click', (event) => {
+        if (!dropdown.contains(event.target)) {
+            dropdown.classList.remove('dropdown-expanded','bill-expand');
+            icon.classList.remove('icon-rotate');
+            menu.classList.remove('menu-open');
+        }
+    });
+});
+
+var tabButtons = document.querySelectorAll(".nav-title");
+
+    for (var i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].addEventListener("click", function () {
+        var tabName = this.dataset.nav;
+        var tabContent = document.getElementById(tabName);
+        var allTabContent = document.querySelectorAll(".desc-content");
+        var allTabButtons = document.querySelectorAll(".nav-title");
+
+        for (var j = 0; j < allTabContent.length; j++) {
+          allTabContent[j].style.display = "none";
+        }
+
+        for (var k = 0; k < allTabButtons.length; k++) {
+          allTabButtons[k].classList.remove("active");
+        }
+
+        tabContent.style.display = "block";
+        this.classList.add("active");
+      });
+    }
+
+    document.querySelector(".nav-title").click();
